c75cf02c169aefdd615acb5ce39eec97918421d2
Mitra Kalloo <Mkalloo42@gmail.com>
2016-03-17 07:03:12 -0400

    post api for forms

--- index.js
+++ index.js
@@ -108,6 +108,80 @@ app.get('/api/:table', function (req, res) {
 
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// getting data from forms
+
+app.post('/Coach', function (req, res) {
+
+  var fstream, data = {};
+  req.pipe(req.busboy);
+
+  // saving the vales in the text field of the form
+  req.busboy.on('field', function (fieldname, val) {
+    if (fieldname == 'fname')
+      data.fname = val;
+    else if (fieldname == 'lname')
+      data.lname = val;
+  });
+
+});
+
+
+
+app.post('/Match', function (req, res) {
+
+  var fstream, data = {};
+  req.pipe(req.busboy);
+
+  // saving the vales in the text field of the form
+  req.busboy.on('field', function (fieldname, val) {
+    if (fieldname == 'playerid')
+      data.playerid = val;
+    else if (fieldname == 'gameid')
+      data.gmaeid = val;
+  });
+
+});
+
+app.post('/Player', function (req, res) {
+
+  var fstream, data = {}, full, thumb;
+  req.pipe(req.busboy);
+
+  // saving the vales in the text field of the form
+  req.busboy.on('field', function (fieldname, val) {
+    if (fieldname == 'fname')
+      data.fname = val;
+    else if (fieldname == 'lname')
+      data.lname = val;
+     else if (fieldname == 'position')
+      data.position = val;
+     else if (fieldname == 'teamid')
+      data.teamid = val;
+  });
+
+});
+
+app.post('/Team', function (req, res) {
+
+  var fstream, data = {}, full, thumb;
+  req.pipe(req.busboy);
+
+  // saving the vales in the text field of the form
+  req.busboy.on('field', function (fieldname, val) {
+    if (fieldname == 'coachid')
+      data.coachid = val;
+    else if (fieldname == 'tname')
+      data.tname = val;
+  });
+
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 app.listen(app.get('port'), function () {
    console.log("Node app is running at localhost:" + app.get('port'));
 });
