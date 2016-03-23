88f03f28c32dc11cd12c9951c4b56e5f65da2cbe
Mitra Kalloo <Mkalloo42@gmail.com>
2016-03-23 17:29:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get ('/api/stats/all',function(req,res){
 });
 
 app.get ('/api/stats/players',function(req,res){
-	connection.query("SELECT DISTINCT Player_Forename , Player_Surname from stats ORDER BY `stats`.`Player_Surname` ASC", function(err, rows){
+	connection.query("SELECT DISTINCT Player_ID, Player_Forename , Player_Surname from stats ORDER BY `stats`.`Player_Surname` ASC", function(err, rows){
     		if (err)
                   throw err;
                 res.json(rows);
